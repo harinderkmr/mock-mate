865d434134b3cac1190393079bf2c119c25e4d1f
harinderkmr <harinderjim@gmail.com>
2025-05-07 21:16:15 +0530

    resend

--- app/dashboard/upgrade/page.jsx
+++ app/dashboard/upgrade/page.jsx
@@ -38,7 +38,7 @@ function Upgrade() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
             </svg>
   
-            <span className="text-gray-700"> 10 users included </span>
+            <span className="text-gray-700"> 5+ Monthly Interviews </span>
           </li>
   
           <li className="flex items-center gap-1">
